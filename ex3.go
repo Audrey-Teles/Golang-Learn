89e5dc19a8c8096acc8f3834400cba6125455667
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-06-10 11:40:30 -0300

    Update ex3.go

--- ex3.go
+++ ex3.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var idade int
+
+	fmt.Println("Informe a sua idade: ")
+	fmt.Scanf("%d", &idade)
+
+	if idade >= 18 {
+		fmt.Println("Você é maior de idade")
+	}
+	else {
+		
+	}
 }
