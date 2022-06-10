64435a05feebb38b0bc5d7d7d2bc4a236de09b4c
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-06-10 12:11:54 -0300

    Update ex4.go

--- ex4.go
+++ ex4.go
@@ -4,12 +4,22 @@ import "fmt"
 
 func main() {
 
-	var altura [5]float
 	var nome [5]string
+	var altura [5]int
 
 	for i := 0; i < 5; i++ {
-		fmt.Println("Informe a sua altura:")
-		fmt.Scanf("%f", &altura)
+		fmt.Println("------- PESSOA ", i+1, " -------")
+
+		fmt.Println("Informe o seu nome: ")
+		fmt.Scanf("%s", &nome[i])
+
+		fmt.Println("Informe a sua altura: (em cm)")
+		fmt.Scanf("%d", &altura[i])
+
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(nome[i], " tem ", altura[i], "cm de altura.")
 	}
 
 }
